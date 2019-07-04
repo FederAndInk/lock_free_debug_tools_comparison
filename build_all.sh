eac8f2f423be2c2f0b374697e275f48c91bc6ffc
FederAndInk <federandink@gmail.com>
2019-07-04 11:11:55 +0200

    build script for gcc/clang/clang+libc++

---
+++ build_all.sh
@@ -0,0 +1,25 @@
+#!/bin/env bash
+
+if [ $(command -v g++) ]; then
+  mkdir build_gcc
+  cd build
+  cmake .. -DCMAKE_CXX_COMPILER=g++
+  cmake --build . -j$(nproc --all)
+  cd ..
+fi
+if [ $(command -v clang++) ]; then
+  mkdir build_clang
+  cd build_clang
+  cmake .. -DCMAKE_CXX_COMPILER=clang++
+  cmake --build . -j$(nproc --all)
+  cd ..
+
+  printf "#include <ciso646>\nint main () {}" | clang -E -stdlib=libc++ -x c++ -dM - | grep _LIBCPP_VERSION
+  if (($? == 0)); then
+    mkdir build_clang_libcxx
+    cd build_clang_libcxx
+    cmake .. -DCMAKE_CXX_COMPILER=clang++ -DUSE_LIBCXX=ON
+    cmake --build . -j$(nproc --all)
+    cd ..
+  fi
+fi
